adb376c2a2bebef9a7c0f26bd2a81c0378cc679c
ilcomarian <ilcomarian@gmail.com>
2018-09-30 00:34:39 -0400

    heroku

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -7,24 +7,20 @@ class ShowController < ApplicationController
   def mp4
     file = YoutubeDL.download params[:url_param]
      
-    
-      
-    
-      
-     
-    # if str.match(/.mp4/) != nil
-    #   str = str.split('.mp4')
-    #   str.push('.mkv')
-    #   str = str.join()
-    # end
-    # name = file.filename
-    # name = name.split('-')
-    # name = name[0] + name[1]
+    str= file.filename
+    if str.match(/.mp4/) != nil
+      str = str.split('.mp4')
+      str.push('.mkv')
+      str = str.join()
+    end
+    name = file.filename
+    name = name.split('-')
+    name = name[0] + name[1]
 
-    # send_data(File.read(file),:filename => file.filename)
+    send_data(File.read(str),:filename => file.filename)
   
-    # File.delete(str)     
-    redirect_to:show_index
+    File.delete(str)     
+     
   end
 
   def mp3
@@ -37,19 +33,19 @@ class ShowController < ApplicationController
 
     
     file =  YoutubeDL.download  params[:url_param], options
-   
-    # str= file.filename
-    # if str.match(/.webm/) != nil
-    # str = str.split('.webm')
-    # str.push('.mp3')
-    # str = str.join()
-    # end
-    # name = file.filename
-    # name = name.split('-')
-    # name = name[0] + name[1]
-    # send_data(File.read(str),:filename => name) 
-    # File.delete(str)   
-    redirect_to:show_index
+    puts file.filename
+    str= file.filename
+    if str.match(/.webm/) != nil
+    str = str.split('.webm')
+    str.push('.mp3')
+    str = str.join()
+    end
+    name = file.filename
+    name = name.split('-')
+    name = name[0] + name[1]
+    send_data(File.read(str),:filename => name) 
+    File.delete(str)   
+    # redirect_to:show_index
   end
   
   def create
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,62 +7,61 @@ class WelcomeController < ApplicationController
  
 
   def search
-    file = YoutubeDL.download params[:query]
-    # # Dennis Lloyd - NEVERMIND
-    # require 'nokogiri'
-    # require 'open-uri'
-    # url       = 'https://youtube.com/results?search_query='
+    # Dennis Lloyd - NEVERMIND
+    require 'nokogiri'
+    require 'open-uri'
+    url       = 'https://youtube.com/results?search_query='
 
-    # # url_text = Net::HTTP.get(url + params[:query])
-    # # page = Nokogiri::XML(url_text)
-    # # puts url_text
-    # page = Nokogiri::HTML(open(url + params[:query])) 
+    # url_text = Net::HTTP.get(url + params[:query])
+    # page = Nokogiri::XML(url_text)
+    # puts url_text
+    page = Nokogiri::HTML(open(url + params[:query])) 
      
     
-    # arr = []
-    # arr2 = []
-    # attr = page.xpath('//a[contains(@href, "watch?")]')
-    # attr.map {|element| arr.push('https://youtube.com' + element["href"])}
+    arr = []
+    arr2 = []
+    attr = page.xpath('//a[contains(@href, "watch?")]')
+    attr.map {|element| arr.push('https://youtube.com' + element["href"])}
  
-    # # options = Selenium::WebDriver::Chrome::Options.new
+    # options = Selenium::WebDriver::Chrome::Options.new
     
-    # # options.add_argument('--disable-extensions')
-    # # options.add_argument('--headless')
-    # # options.add_argument('--disable-gpu')
-    # # options.add_argument('--no-sandbox')
-    # # options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument('--disable-extensions')
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-gpu')
+    # options.add_argument('--no-sandbox')
+    # options.add_argument('--disable-dev-shm-usage')
      
    
      
-    # # driver = Selenium::WebDriver.for :chrome,options: options
+    # driver = Selenium::WebDriver.for :chrome,options: options
 
-    # # driver.manage.window.resize_to(1300, 500)
+    # driver.manage.window.resize_to(1300, 500)
   
-    # #  query     = CGI.escape(params[:query])
-    # #  url       = 'http://youtube.com/results?search_query='
-    # #  page      = driver.navigate.to(url + query)
+    #  query     = CGI.escape(params[:query])
+    #  url       = 'http://youtube.com/results?search_query='
+    #  page      = driver.navigate.to(url + query)
      
-    # #  u = driver.find_elements(:xpath, '//a[contains(@href, "watch?")]')
-    #  # driver.quit
-    # $video_url = []
-    # $file = []
-    # (0..24).each do |i|
-    #   next unless i.even?
+    #  u = driver.find_elements(:xpath, '//a[contains(@href, "watch?")]')
+     # driver.quit
+    $video_url = []
+    $file = []
+    (0..24).each do |i|
+      next unless i.even?
    
-    #   str = arr[i]
+      str = arr[i]
       
-    #   puts arr[i]
+      puts arr[i]
     
-    #   $file.push(str)
-    #   str = str.split('/')
-    #   str[3] = str[3].split('?v=')
-    #   str[3][0] = 'embed'
-    #   str = str.join('/')
-    #   $video_url.push(str)
+      $file.push(str)
+      str = str.split('/')
+      str[3] = str[3].split('?v=')
+      str[3][0] = 'embed'
+      str = str.join('/')
+      $video_url.push(str)
     
-    # end
+    end
 
-    # redirect_to :show_index
+    redirect_to :show_index
   end
  
    
